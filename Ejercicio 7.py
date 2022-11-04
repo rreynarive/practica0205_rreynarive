736aeaab520db45d36b052c3fa8260a82d2d1f0c
Rosa <rreynarive1@educacion.navarra.es>
2022-11-04 18:51:06 +0100

    Ejercicio 6 y 7 faltan

---
+++ Ejercicio 7.py
@@ -0,0 +1,6 @@
+# Escribir un programa que almacene el abecedario
+# en una lista, elimine de la lista las letras que
+# ocupen posiciones múltiplos de 3, y muestre por
+# pantalla la lista resultante.
+list_abec= list("abcdefghijklmnñopqrstuvwxyz")
+print(list_abec[0:24:2])

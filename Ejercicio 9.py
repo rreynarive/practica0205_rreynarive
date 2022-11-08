dab5cd321b9ef1e8b46c5b943d88d3b8af009445
Rosa Reyna <rreynarive1@educacion.navarra.es>
2022-11-08 12:42:50 +0100

    Faltan 6, 8, 9, 11, 12

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -1,2 +1,8 @@
 # Escribir un programa que pida al usuario una palabra
 # y muestre por pantalla el número de veces que contiene cada vocal.
+palabra = input("Introduzca una palabra:\n")
+list_vocal = list("aeiou")
+for vocal in list_vocal:
+    for i in palabra:
+        if i == vocal:
+            print(i)

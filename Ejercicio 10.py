0db7fe2a5435436e5e569c899b873ef36f602e95
Rosa Reyna <rreynarive1@educacion.navarra.es>
2022-11-08 12:10:48 +0100

    Ejercicio 10 FIN

---
+++ Ejercicio 10.py
@@ -0,0 +1,7 @@
+# Escribir un programa que almacene en una lista los
+# siguientes precios, 50, 75, 46, 22, 80, 65, 8, y muestre por
+# pantalla el menor y el mayor de los precios.
+lis_precio = [50, 75, 46, 22, 80, 65, 8]
+
+print("Precio menor: ", min(lis_precio))
+print("Precio mayor: ", max(lis_precio))
---
+++ Ejercicio 12.py
@@ -0,0 +1,1 @@
+# Escribir un programa que almacene las matrices:

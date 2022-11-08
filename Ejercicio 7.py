e2c198bb4b0b6c3509896c496c2e3d62294f8904
Rosa Reyna <rreynarive1@educacion.navarra.es>
2022-11-08 11:22:15 +0100

    Ejercicio 7 FIN

--- Ejercicio 7.py
+++ Ejercicio 7.py
@@ -2,5 +2,8 @@
 # en una lista, elimine de la lista las letras que
 # ocupen posiciones múltiplos de 3, y muestre por
 # pantalla la lista resultante.
-list_abec= list("abcdefghijklmnñopqrstuvwxyz")
-print(list_abec[0:24:2])
+list_abec = list("abcdefghijklmnñopqrstuvwxyz")
+for i in range(len(list_abec), 0, -1):
+    if i % 3 == 0:
+        list_abec.pop(i-1)
+print(list_abec)
